7eb8be391c8b8108df46d4c499a7d13bc53bd690
Swastik Roy <swastik.roy@live.com>
2018-03-30 01:51:21 -0700

    Update extraction_methods.m

--- extraction_methods.m
+++ extraction_methods.m
@@ -1,1 +1,9 @@
 
+% Helper methods from Matlab
+
+% Fast fourier transform
+% Y = fft(X)  ----> . Converts time series data frequency domain data
+
+% Discrete Fourier Transform
+% [cA,cD] = dwt(X,'wname') ----> approximation coefficients vector cA and detail coefficients vector cD, wname is wavelet name
+% https://www.mathworks.com/help/wavelet/ref/dwt.html https://www.mathworks.com/help/wavelet/ref/wfilters.html
