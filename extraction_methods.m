a6e07a2c9e4776772053c3bb7a82f8e0c799112e
Ankit Rajshree <arajshre@asu.edu>
2018-03-30 15:50:58 -0700

    Update extraction_methods.m

--- extraction_methods.m
+++ extraction_methods.m
@@ -18,3 +18,25 @@ function feature = zero_crossing_rate(X)
   feature = sum(abs(diff(X>0)))/length(X);
 end
 
+function feature = waveform_length(X)
+  feature = sum(abs(diff(X)));
+end
+
+% Root mean square
+function feature = root_mean_square(X)
+  feature = rms(s);
+end
+
+% Mean of power in frequency domain TODO - Explain in a better way
+function feature = mean_power_f(X)
+  y = fft(X);
+  power = abs(y).^2/length(y);
+  feature = mean(power);
+end
+
+% Standard Deviation of power TODO - Explain in a better way
+function feature = sd_power_f(X)
+  y = fft(X);
+  power = abs(y).^2/length(y);
+  feature = std(power);
+end
