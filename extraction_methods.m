e193bad7355a30483c688720004edfb3ebcbfd01
Ankit Rajshree <arajshre@asu.edu>
2018-03-30 15:44:24 -0700

    Add zero crossing rate feature extraction method

--- extraction_methods.m
+++ extraction_methods.m
@@ -8,6 +8,13 @@
 % [cA,cD] = dwt(X,'wname') ----> approximation coefficients vector cA and detail coefficients vector cD, wname is wavelet name
 % https://www.mathworks.com/help/wavelet/ref/dwt.html https://www.mathworks.com/help/wavelet/ref/wfilters.html
 
+% Maximum frequency from FFT Output
 function result = maxFFT(X)
   result = max(fft(X));
 end
+
+% Zero crossing rate
+function feature = zero_crossing_rate(X)
+  feature = sum(abs(diff(X>0)))/length(X);
+end
+
