3df43000c07b480b7eebeb692c13b4bc664fecac
Ankit Rajshree <arajshre@asu.edu>
2018-03-30 16:00:43 -0700

    Update plot-util.m

--- plot-util.m
+++ plot-util.m
@@ -1,4 +1,4 @@
-% Contains methods to plot features from sensors like accelerometer,gyroscope, emg, etc.
+% Contain methods to plot features from sensors like accelerometer,gyroscope, emg, etc.
 
 % Matlab helper plot functions
 % histogram(X) --> https://www.mathworks.com/help/matlab/ref/matlab.graphics.chart.primitive.histogram.html;jsessionid=9226ed507821d77570150e876aa8
