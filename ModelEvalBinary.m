f7f2d8f617f8d68a625e9dc7906a58e54f3b505f
ankitrajshree <arajshre@asu.edu>
2018-04-11 15:24:48 -0700

    Modified ModalEvalBinary for storing accuracy scores in csv

--- ModelEvalBinary.m
+++ ModelEvalBinary.m
@@ -1,5 +1,16 @@
 % read in data
 data_tmp = dlmread('pca_feature.csv');
+baseDir = pwd;
+mkdir('Accuracy');
+%Storing the generated Accuracy, Precision, Recall , F1_Score
+svmFile = fullfile(baseDir,'Accuracy','SVM_Metrics.csv');
+nnFile = fullfile(baseDir,'Accuracy','NN_Metrics.csv');
+dtFile = fullfile(baseDir,'Accuracy','DT_Metrics.csv');
+metrics = ["Accuracy","Precision","Recall","F1_Score"];
+xlswrite(svmFile,metrics);
+xlswrite(svmFile,nnFile);
+xlswrite(svmFile,dtFile);
+
 X_train = [];
 X_test = [];
 y_train = [];
@@ -29,9 +40,12 @@ for i = 1:10
     y_test2(y_test2 ~= classNames(i)) = 0; % Create binary classes for each classifier
     y_test2(y_test2 == classNames(i)) = 1;
     [acc,precision,recall, f1score] = SVM(X_train,y_train_2,X_test,y_test2, i);
+    xlswrite(svmFile,[acc,precision,recall, f1score])
     
     [acc,precision,recall, f1score] = NNet([X_train; X_test] ,[y_train_2 ;y_test2]);
+    xlswrite(nnFile,[acc,precision,recall, f1score])
     
     [acc,precision,recall, f1score] = DecisionTree(X_train,y_train_2,X_test,y_test2);
+    xlswrite(dtFile,[acc,precision,recall, f1score])
 end
 
