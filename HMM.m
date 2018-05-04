c34a12ebc833a63f95ddf8a056da176ebed92008
arpitmohanty9 <arpitmohanty9@gmail.com>
2018-05-03 20:50:48 -0700

    Adding HMM file

---
+++ HMM.m
@@ -0,0 +1,19 @@
+function [likelystates,accuracy] = HMM(trans_mat,emis_mat)
+
+% we use hmmgenerate to  generate a random sequence of states and emissions
+% of the model
+
+[seq,states] = hmmgenerate(1000,TRANS,EMIS);
+
+% seq = sequence of emissions , states = sequence of states.
+
+%Given the transition and emission matrices TRANS and EMIS, the function hmmviterbi 
+%uses the Viterbi algorithm to compute the most likely sequence of states the model 
+%would go through to generate a given sequence seq of emissions.
+
+likelystates = hmmviterbi(seq, TRANS, EMIS);
+
+accuracy = sum(states==likelystates)/1000;
+
+end
+
